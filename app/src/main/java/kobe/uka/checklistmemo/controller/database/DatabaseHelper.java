d761be15cffd9b31a0d2bf32f5f5413a572ecda4
kobeuk <k08euk@gmail.com>
2017-04-27 15:08:41 +0900

    first commit

---
+++ app/src/main/java/kobe/uka/checklistmemo/controller/database/DatabaseHelper.java
@@ -0,0 +1,8 @@
+package kobe.uka.checklistmemo.controller.database;
+
+/**
+ * Created by uka on 2017/04/27.
+ */
+
+public class DatabaseHelper {
+}
---
+++ app/src/main/java/kobe/uka/checklistmemo/view/MemoListActivity.java
@@ -0,0 +1,15 @@
+package kobe.uka.checklistmemo.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kobe.uka.checklistmemo.R;
+
+public class MemoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.memo_list);
+    }
+}
